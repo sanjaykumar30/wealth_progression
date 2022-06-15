2b72e6f18fd475f8094502cf6ef199e77467bd08
sanjaykumar30 <m.sanjaykumar3072002@gamil.com>
2022-06-15 21:18:48 +0530

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
-mkdir -p ~/.streamlit/echo "\
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"m.sanjaykumar3072002@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
 [server]\n\
 headless = true\n\
+enableCORS=false\n\
 port = $PORT\n\
-enableCORS = false\n\
-\n\
 " > ~/.streamlit/config.toml
